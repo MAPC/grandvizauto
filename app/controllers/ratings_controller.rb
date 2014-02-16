54dcd901ec56c916e29159f9386732bdee86d8f3
Matt Cloyd <cloydster@gmail.com>
2014-02-15 19:25:07 -0500

    Add link to edit profile. I put all the user actions into one dropdown, for compactness.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -4,11 +4,7 @@ class RatingsController < ApplicationController
     @rating     = Rating.find(params[:id])
     @submission = @rating.submission
 
-    puts "SENT SCORE: #{params[:score]}"
-
     if @rating.update_attributes(score: params[:score])
-      @rating.reload
-      puts "RELOADED WITH: #{@rating.score}"
       respond_to do |format|
         format.js
       end
