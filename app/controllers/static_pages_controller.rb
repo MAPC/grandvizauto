38fd1377e9533846490d3370628befc18b4bf5cf
Matt Cloyd <cloydster@gmail.com>
2014-02-17 10:41:52 -0500

    Set up download as a controller action, so the URL remains invisible

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,13 @@ class StaticPagesController < ApplicationController
 
   def challenge
   end
+
+  def download
+    if current_user.update_attribute(:agreed, true)
+      data = open('https://s3.amazonaws.com/files.hubwaydatachallenge.org/updated_hubway_station_list_2014.csv')
+      send_data data.read, type: data.content_type, x_sendfile: true
+    else
+      redirect_to root_url, notice: "Could not mark you as having accepted the terms, so refusing to download the file."
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140215192211) do
+ActiveRecord::Schema.define(:version => 20140216011112) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(:version => 20140215192211) do
     t.string   "provider"
     t.string   "uid"
     t.string   "email"
+    t.boolean  "agreed"
   end
 
 end
