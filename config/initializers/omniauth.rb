f9b6e64f9771a11a340c376372a70644299d9758
Matt Cloyd <cloydster@gmail.com>
2014-02-17 14:16:48 -0500

    Add Twitter as oauth provider

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github,          ENV['GITHUB_CLIENT_ID'],   ENV['GITHUB_CLIENT_SECRET']
   provider :google_oauth2,   ENV['GOOGLE_CLIENT_ID'],   ENV['GOOGLE_CLIENT_SECRET']
   provider :facebook,        ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET']
+  provider :twitter,         ENV['TWITTER_CLIENT_KEY'],  ENV['TWITTER_CLIENT_SECRET']
 end
 
