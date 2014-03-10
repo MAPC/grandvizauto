e41a9bf4a00e92e5d8d88fb70b4430604f13f971
Matt Cloyd <cloydster@gmail.com>
2014-03-10 17:22:40 -0400

    Make the confirm email more verbose, and redirect_to instead of send_data for the giant AWS file

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -22,8 +22,7 @@ class StaticPagesController < ApplicationController
 
   def download
     if current_user.update_attribute(:agreed, true)
-      data = open(ENV['GVA_DATA_URL'])
-      send_data data.read, type: data.content_type, x_sendfile: true
+      redirect_to ENV['GVA_DATA_URL']
     else
       redirect_to root_url, notice: "Could not mark you as having accepted the terms, so refusing to download the file."
     end
